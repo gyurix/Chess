2e3bab1ec00b601c8da7e86ad04466f635b84bfe
gyurix <barathgy@gmail.com>
2019-02-14 13:02:48 +0100

    Added main class and improved README

---
+++ src/main/java/barnes/chess/Chess.java
@@ -0,0 +1,10 @@
+package barnes.chess;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Chess extends Application {
+  public void start(Stage primaryStage) throws Exception {
+
+  }
+}
