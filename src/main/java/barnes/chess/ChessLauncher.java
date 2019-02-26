d38a6e269c3c98171e7a4d35f31eef2c2f30e2b4
Gyurix <barathgy@gmail.com>
2019-02-26 22:38:32 +0100

    Removed useless throws Exception part

--- src/main/java/barnes/chess/ChessLauncher.java
+++ src/main/java/barnes/chess/ChessLauncher.java
@@ -12,7 +12,7 @@ public class ChessLauncher extends Application {
   }
 
   @Override
-  public void start(Stage primaryStage) throws Exception {
+  public void start(Stage primaryStage) {
     System.out.println("Started");
     DB db = new DB(DatabaseConfig.builder()
             .host("142.93.101.191")
